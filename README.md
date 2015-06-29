b5dc033f5c650a7c00694ce29921980ef8815439
dungnv53 <vandung_17@yahoo.com>
2015-06-29 20:55:01 +0700

    remove wrong source

--- README.md
+++
@@ -1,4 +0,0 @@
-# Android_GCM_sample
-
-Source from https://github.com/googlesamples/google-services.git
-But only gcm service on android.
--- app/src/main/java/et/littlewing/com/ig_pushnotice/CommonUtilities.java
+++
@@ -1,9 +0,0 @@
-package et.littlewing.com.ig_pushnotice;
-
-/**
- * Created by nickfarrow on 6/26/15.
- */
-public class CommonUtilities {
-
-    static final String SENDER_ID = "225016095470"; // your project number from GCM
-}
